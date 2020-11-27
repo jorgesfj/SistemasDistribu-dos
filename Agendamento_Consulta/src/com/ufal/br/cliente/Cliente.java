4541c33b46f23bfb3e3cd9b49fafcb6e4d6e7d62
Jorge Soares <junior-soares42@hotmail.com>
2020-11-27 13:21:16 -0300

    Adicionando modelo da consulta e método para criar

--- Agendamento_Consulta/src/com/ufal/br/cliente/Cliente.java
+++ Agendamento_Consulta/src/com/ufal/br/cliente/Cliente.java
@@ -3,22 +3,47 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.ufal.br.model.Consulta;
+
 public class Cliente {
+	public Consulta agendarConsulta(int id, String nomePaciente, int dia, int hora, int minuto, String descricao) {
+		Consulta consulta = new Consulta(id, nomePaciente, dia, hora, minuto, descricao);
+		
+		return consulta;
+	}
 	
 	public static void main(String[] args) {
 		
 		try{
 			//criando o socket para rodar na máquina local(localhost) e na porta 5555
 			Socket socket = new Socket("localhost", 5555);
-			
+			Cliente cliente = new Cliente();
 			//instânciando o recebimento
 			ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
 			//instânciando o envio
 			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+			Scanner ler = new Scanner(System.in);
 			
+			System.out.println("Digite o Seu nome: ");
+			String nomePaciente = ler.next();
+			
+			System.out.println("Digite o dia que você quer se consultar: ");
+			int dia = ler.nextInt();
+			
+			System.out.println("Digite a Hora da consulta: (só o inteiro) ");
+			int hora = ler.nextInt();
+			
+			System.out.println("Digite qual minuto começará a consulta: ");
+			int minuto = ler.nextInt();
+			
+			System.out.println("Descreva a colsulta: ");
+			String descricao = ler.next();
+			
+			Consulta consulta = cliente.agendarConsulta(0, nomePaciente, dia, hora, minuto, descricao);
 			//fechando o envio
 			input.close();
 			//fechando o recebimento
--- Agendamento_Consulta/src/com/ufal/br/model/Consulta.java
+++ Agendamento_Consulta/src/com/ufal/br/model/Consulta.java
@@ -1,5 +1,66 @@
 package com.ufal.br.model;
 
 public class Consulta {
+	
+	private int id;
+	private String nomePaciente;
+	private int dia;
+	private int hora;
+	private int minuto;
+	private String descricao;
+	public Consulta (int id, String nomePaciente, int dia, int hora, int minuto, String descricao) {
+		this.id = id;
+		this.nomePaciente = nomePaciente;
+		this.dia = dia;
+		this.hora = hora;
+		this.minuto = minuto;
+		this.descricao = descricao;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNomePaciente() {
+		return nomePaciente;
+	}
+	public void setNomePaciente(String nomePaciente) {
+		this.nomePaciente = nomePaciente;
+	}
+	
+	public int getDia() {
+		return dia;
+	}
 
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getHora() {
+		return hora;
+	}
+
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	public int getMinuto() {
+		return minuto;
+	}
+
+	public void setMinuto(int minuto) {
+		this.minuto = minuto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
 }
