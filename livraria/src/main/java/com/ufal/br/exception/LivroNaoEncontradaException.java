9bb555cb82d2f41ca93ac74d655802f389c40885
Jorge Soares <junior-soares42@hotmail.com>
2021-01-16 21:39:46 -0300

    deletando

--- livraria/src/main/java/com/ufal/br/exception/LivroNaoEncontradaException.java
+++
@@ -1,9 +0,0 @@
-package com.ufal.br.exception;
-
-public class LivroNaoEncontradaException extends Exception {
-	
-	public LivroNaoEncontradaException(long id) {
-		super("Não foi possível encontrar o livro com o id: " + id);
-			}
-
-}
